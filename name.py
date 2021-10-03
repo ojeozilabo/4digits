d836976d8811efb27a99583ff421c442ed6f9131
Kelvin Adeoye <bambowork@ojeozilabos-MacBook-Pro.local>
2021-10-03 17:40:39 +0100

    Chhanges

---
+++ name.py
@@ -0,0 +1,9 @@
+#This program will add two numbers
+
+a = ()
+b = ()
+print('Type first no here')         #ask user to type np
+a = float(input())                  #collects integer and turns it a float
+print ('Type second no here')
+b = float(input())
+print('The result is ' + str(a + b))    #prints out the addition of the first no and the second no
