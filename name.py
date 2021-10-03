52185ce14f8a73f38e33d480555819e4a4a0d811
Kelvin Adeoye <bambowork@ojeozilabos-MacBook-Pro.local>
2021-10-03 17:44:40 +0100

    modified name.py

--- name.py
+++ name.py
@@ -6,4 +6,4 @@ print('Type first no here')         #ask user to type np
 a = float(input())                  #collects integer and turns it a float
 print ('Type second no here')
 b = float(input())
-print('The result is ' + str(a + b))    #prints out the addition of the first no and the second no
+print('The result is ' + str(a + b))    #prints out the addition of the first no and the second number
